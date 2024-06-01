6d8d949384fa2229ff2806be7fed78030817d75a
prabhootml <prabhootpatel@gmail.com>
2024-06-01 23:20:32 +0530

    all set

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GlobalProvider } from './context/globalContext';
+import { GlobalStyle } from './styles/GlobalStyle';
+
+<script src="https://cdn.lordicon.com/lordicon.js"></script>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
+  </React.StrictMode>
+);
+
