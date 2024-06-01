d11bb71a0b3e0f71922aeb92c8d5f62ce35f221f
prabhootml <prabhootpatel@gmail.com>
2024-06-02 00:20:06 +0530

    rt

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,28 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const { db } = require('./config/db');
-const {readdirSync} = require('fs')
-const app = express()
+const { readdirSync } = require('fs');
+const app = express();
 const path = require('path');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT 
-const CORS_URL=process.env.CORS_URL
+const PORT = process.env.PORT;
+const CORS_URL = process.env.CORS_URL;
 //middlewares
-app.use(express.json())
-app.use(cors({
-  origin:["https://expense-tracker-2b3u.vercel.app/"],
-  methods:["POST,GET,DELETE"],
-  credentials:true
-}))
+app.use(express.json());
+app.use(
+  cors({
+    origin: ['https://expense-tracker-2b3u.vercel.app'],
+    methods: ['POST,GET,DELETE'],
+    credentials: true,
+  })
+);
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+readdirSync('./routes').map((route) =>
+  app.use('/api/v1', require('./routes/' + route))
+);
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
@@ -29,12 +33,10 @@ app.listen(9000);
 console.log(__dirname);
 
 const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
-}
+  db();
+  app.listen(PORT, () => {
+    console.log('listening to port:', PORT);
+  });
+};
 
-
-server()
-
+server();
