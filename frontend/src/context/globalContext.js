e05c7b4fce0529cfc11268a4240e727fd17e2b67
prabhu <93818934+prabhoot@users.noreply.github.com>
2024-06-01 19:31:33 +0000

    final

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,10 @@
 import React, { useContext, useState } from "react"
 import axios from 'axios'
 
-const BASE_URL = "http://localhost:4000/api/v1/";
+// const BASE_URL = "http://localhost:4000/api/v1/";
+const BASE_URL = "https://expense-tracker-2-p2e2.onrender.com/api/v1/";
+// require('dotenv').config()
+// const BASE_URL = process.env.BASE_URL;
 
 const GlobalContext = React.createContext()
 
