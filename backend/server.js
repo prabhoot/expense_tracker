7fd4ec7f55e196e6705680a8039e99594aee2308
prabhu <93818934+prabhoot@users.noreply.github.com>
2024-06-01 18:38:16 +0000

    another

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,12 @@ readdirSync('./routes').map(route =>
 );
 
 // Serve frontend build files
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Serve React app for any other route
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
 
 // Start server
 const server = () => {
